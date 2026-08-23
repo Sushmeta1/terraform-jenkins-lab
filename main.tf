e65af06ad9412f11b9f59ae495e3ffeb952c418d
Sushmeta Negi <sushmetanegi@gmail.com>
2026-08-24 00:06:29 +0530

    Test insecure SSH rule

--- main.tf
+++ main.tf
@@ -1,4 +1,17 @@
 resource "aws_instance" "example" {
-ami          = var.ami
+  ami           = var.ami
   instance_type = var.instance_type
+}
+
+resource "aws_security_group" "ssh_test" {
+  name = "ssh-test"
+
+  ingress {
+    description = "Allow SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
