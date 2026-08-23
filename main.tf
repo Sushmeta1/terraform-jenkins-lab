2856714519d15cb78be424ebd03745bf68241488
Sushmeta Negi <sushmetanegi@gmail.com>
2026-08-24 01:45:07 +0530

    Initialize Terraform with AWS provider
    Add Terraform configuration with required providers

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = var.ami
   instance_type = var.instance_type
