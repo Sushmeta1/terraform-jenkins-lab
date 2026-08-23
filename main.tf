d7f02615eafcd444d0456fee55bc10fa7fc8fb1d
Sushmeta Negi <sushmetanegi@gmail.com>
2026-08-24 01:23:32 +0530

    Modify aws_instance resource for security enhancements
    Updated AWS instance resource with metadata options and root block device encryption.

--- main.tf
+++ main.tf
@@ -1,15 +1,12 @@
-terraform {
-  required_version = ">= 1.0"
-
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 6.0"
-    }
-  }
-}
-
 resource "aws_instance" "example" {
   ami           = var.ami
   instance_type = var.instance_type
+
+  metadata_options {
+    http_tokens = "required"
+  }
+
+  root_block_device {
+    encrypted = true
+  }
 }
